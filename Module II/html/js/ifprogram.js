06837bbc9a36de376c588c6ca007a4323c40629d
Harshavardhanan123 <harshvardhanan5@gmail.com>
2023-06-28 12:48:46 +0530

    6/28/2023

---
+++ Module II/html/js/ifprogram.js
@@ -0,0 +1,17 @@
+a= 20
+if(a>80)
+{
+    console.log("A Grade")
+}
+else if(a>60)
+{
+    console.log("B Grade")
+}
+else if(a>40)
+{
+    console.log("C Grade")
+}
+else
+{
+    console.log("Failed")
+}
