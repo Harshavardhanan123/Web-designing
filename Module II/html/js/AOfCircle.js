05232920f5a5bc4f319c9687b00540d33d093bd4
Harshavardhanan123 <harshvardhanan5@gmail.com>
2023-07-03 09:22:24 +0530

    3/7/2023

---
+++ Module II/html/js/AOfCircle.js
@@ -0,0 +1,7 @@
+function calculateArea(radius) {
+    return Math.PI * Math.pow(radius, 2);
+    }
+    var radius = 4;
+    var area = calculateArea(radius);
+    console.log
+    ("The area of the circle is: " + area);
